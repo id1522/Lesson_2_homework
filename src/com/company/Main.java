3c227a9d95e74d39e0040f86b609eed064d568c2
Nikita Smorschok <nsmorschok@gmail.ru>
2022-02-19 17:57:27 +0300

    Name<user>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("Введите имя полбзователя:");
+
+        Scanner name = new Scanner(System.in);
+        String phrase1 = name.nextLine();
+        System.out.print("Здравствуйте ");
+        System.out.println(phrase1);
+
     }
 }
